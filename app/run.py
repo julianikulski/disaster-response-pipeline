dff12b6aa1eccad11f78ac98079ac7890c417b7e
JuliaNikulski <julia.nikulski@gmail.com>
2019-06-09 16:16:45 +0000

    fix: Remove binary parameter from gridsearch

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
+engine = create_engine('sqlite:///../data/')
 df = pd.read_sql_table('YourTableName', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
